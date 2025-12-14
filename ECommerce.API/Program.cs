5d5cabbb956562465453defb160d909082848ba8
gokhan <bayraktutargokhan@gmail.com>
2025-12-15 00:02:21 +0300

    Favourite_Product

--- ECommerce.API/Program.cs
+++ ECommerce.API/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IFavourite_ProductService, Favourite_ProductService>();
 
 // =======================
 // JWT AUTH
